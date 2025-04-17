05fea99350a84b77983175e8b7cb0013b99d1457
김승찬 <tmdcks801>
2025-04-17 10:13:28 +0900

    feat : 초기 프로젝트 업로드

---
+++ src/main/java/com/example/part35teammonew/Part35teamMonewApplication.java
@@ -0,0 +1,13 @@
+package com.example.part35teammonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part35teamMonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part35teamMonewApplication.class, args);
+	}
+
+}
