06dd71c64bb32533938e63b2702583101f3908b4
kbh <xjvm7001@naver.com>
2025-04-18 13:06:44 +0900

    refactor: @Getter 추가

--- src/main/java/com/example/part35teammonew/domain/ArticleInterest.java
+++ src/main/java/com/example/part35teammonew/domain/ArticleInterest.java
@@ -12,9 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "article_interests")
+@Getter
 public class ArticleInterest {
 
 	@Id
