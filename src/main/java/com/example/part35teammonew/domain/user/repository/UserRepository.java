e58196163d29d1a563e4069fc6a0a549094e6d8c
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-04-21 09:08:55 +0900

    test: user 레포지토리 단위테스트

--- src/main/java/com/example/part35teammonew/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/part35teammonew/domain/user/repository/UserRepository.java
@@ -4,9 +4,13 @@ import com.example.part35teammonew.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface userRepostiory extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmailAndIsDeletedFalse(String email);
+
 
 }
