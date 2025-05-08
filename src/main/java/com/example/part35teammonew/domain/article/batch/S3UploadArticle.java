0435facc1e8bf8cca113e8bd9decaf8db28c2d19
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-05-08 13:20:02 +0900

    pipeline: prod 프로필시 서버 실행을 위한 각종 설정 수정
    # Conflicts:
    #	articles_2025-05-08.json

--- src/main/java/com/example/part35teammonew/domain/article/batch/S3UploadArticle.java
+++ src/main/java/com/example/part35teammonew/domain/article/batch/S3UploadArticle.java
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("dev")
+@Profile({"dev", "prod"})
 @Service
 @RequiredArgsConstructor
 @Slf4j
